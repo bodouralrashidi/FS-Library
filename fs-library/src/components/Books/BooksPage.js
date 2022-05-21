2d61593de36aceb238d7eeaa8dbd8e457cab6bc5
bodouralrashidi <bodouralrashidi@gmail.com>
2022-05-21 20:58:06 +0300

    merge

--- fs-library/src/components/Books/BooksPage.js
+++ fs-library/src/components/Books/BooksPage.js
@@ -4,25 +4,19 @@ import BookList from "./BookList";
 import AddBookModal from "./AddBookModal";
 
 function BooksPage() {
-<<<<<<< HEAD
   // const [isOpen, setIsOpen] = useState(false);
 
   //const closeModal = () => setIsOpen(false);
 
   // const openModal = () => setIsOpen(true);
-=======
   
 
->>>>>>> 3468b61 (add button works)
   return (
     <div class="bookPage">
       <center>
-<<<<<<< HEAD
         <h1 style={{ color: "#7678ED" }}>شنو نكتب اهني</h1>
-=======
         <h1>Archive Library Book</h1>
         
->>>>>>> 3468b61 (add button works)
         <BookList />
         
 
--- fs-library/src/components/members/MembersPage.js
+++ fs-library/src/components/members/MembersPage.js
@@ -14,13 +14,7 @@ function MembersPage() {
   return (
     <div class="memberPage">
       <center>
-<<<<<<< HEAD
-        <h1 style={{ color: "#7678ED", marginTop: "30px" }}>
-          Rkive Library Members
-        </h1>
-=======
         <h1 style={{ color: "#7678ED" }}> Archive Library Members</h1>
->>>>>>> 3468b61 (add button works)
         <MemberList />
         <button class="addMember-btn">
           <span onClick={openModal}> + </span>
